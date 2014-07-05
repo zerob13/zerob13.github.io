3924534bf87491275f66b98f3f533c30dbf26041
zerob13 <zerob13@gmail.com>
2014-07-05 16:35:18 +0800

    add mobile support

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,27 @@
 $(document).ready(function(){
+    var isMobile = {  
+        Android: function() {  
+            return navigator.userAgent.match(/Android/i) ? true : false;  
+        },  
+BlackBerry: function() {  
+    return navigator.userAgent.match(/BlackBerry/i) ? true : false;  
+},  
+iOS: function() {  
+    return navigator.userAgent.match(/iPhone|iPad|iPod/i) ? true : false;  
+},  
+Windows: function() {  
+    return navigator.userAgent.match(/IEMobile/i) ? true : false;  
+},  
+any: function() {  
+    return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Windows());  
+}  
+};  
+if( isMobile.any() )   
+{  
+    $('#avatar').addClass('swing');                         
+}  else{
     $('#avatar').hover(function(){
         $(this).addClass('swing');
     },function(){$(this).removeClass('swing')});
-
+}
 });
