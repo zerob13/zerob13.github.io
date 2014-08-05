58f9159e362264c30fbd83d400175bc665c1dc84
zerob13 <zerob13@gmail.com>
2014-08-05 10:43:14 +0800

    add some shamate feature

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,6 +31,20 @@ if( isMobile.any() )
             tilt([orientation.x * 50, orientation.y * 50]);
         }, true);
     }
+    count=0;
+    fa=$('.fa');
+    window.setInterval(function(){
+        if(count>=fa.length){
+            count=0;
+        }
+        $(fa[count]).addClass('sh-text').css("color","#ff9800");
+        if(count==0){
+            $(fa[fa.length-1]).removeClass('sh-text').css("color","#b2dfdb");
+        }else{
+            $(fa[count-1]).removeClass('sh-text').css("color","#b2dfdb");
+        }
+        count++;
+    },1000);
 }  else{
     $('#avatar').bind('touchstart mousedown', function(){ $(this).addClass('swing').addClass('sh-img');
     },function(){$(this).removeClass('swing').removeClass('sh-img') });
@@ -48,10 +62,11 @@ if( isMobile.any() )
 id = -1;
 function tilt(x){
     // console.log(x[1]);
-    $('#avatar').css("-webkit-transform","rotate("+x[1]+"deg)");
-    $('#avatar').css("-moz-transform","rotate("+x[1]+"deg)");
-    $('#avatar').css("-ms-transform","rotate("+x[1]+"deg)");
-    $('#avatar').css("-o-transform","rotate("+x[1]+"deg)");
-    $('#avatar').css("transform","rotate("+x[1]+"deg)");
+    arc=-x[1]/10;
+    $('#avatar').css("-webkit-transform","rotate("+arc+"deg)");
+    $('#avatar').css("-moz-transform","rotate("+arc+"deg)");
+    $('#avatar').css("-ms-transform","rotate("+arc+"deg)");
+    $('#avatar').css("-o-transform","rotate("+arc+"deg)");
+    $('#avatar').css("transform","rotate("+arc+"deg)");
 }
 
